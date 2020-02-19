b82c16b7d9d8d671aa242bd490068ec9d9a85dd8
Miroslav Bozik <mirrobozik@gmail.com>
2020-02-19 11:54:57 +0100

    Added Razor project to the solution

---
+++ src/Razor/EmailConfigurationBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using CASPR.Extensions.Email.TemplateEngines;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CASPR.Extensions.Email
+{
+    public static class EmailConfigurationBuilderExtensions
+    {
+        /// <summary>
+        /// Add Razor template engine
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IEmailConfigurationBuilder AddRazorTemplateEngine(
+            this IEmailConfigurationBuilder builder)
+        {
+            var descriptor = ServiceDescriptor.Transient<IEmailTemplateEngine, NullEmailTemplateEngine>();
+            builder.Services.Remove(descriptor);
+            builder.Services.AddSingleton<IEmailTemplateEngine, RazorTemplateEngine>();
+            return builder;
+        }
+    }
+}
---
+++ src/Razor/TemplateEngines/RazorTemplateEngine.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using RazorLight;
+
+namespace CASPR.Extensions.Email.TemplateEngines
+{
+    public class RazorTemplateEngine : IEmailTemplateEngine
+    {
+        private readonly RazorLightEngine _engine;
+
+        public RazorTemplateEngine()
+        {
+            _engine = new RazorLightEngineBuilder()
+                .UseMemoryCachingProvider()
+                .Build();
+        }
+
+        public async Task<string> RenderAsync<T>(
+            string templateKey, 
+            string templateContent, T model, bool isHtml = true)
+        {
+            var cacheResult = _engine.TemplateCache.RetrieveTemplate(templateKey);
+            if (cacheResult.Success)
+            {
+                string result = await _engine.RenderTemplateAsync(cacheResult.Template.TemplatePageFactory(), model);
+                return result;
+            }
+            return await _engine
+                .CompileRenderAsync(templateKey, templateContent, model);
+        }
+    }
+}
